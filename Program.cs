c63eded77e96da673204a4ea105214c9d20fb316
Asus <koncarik@mail.ru>
2023-01-12 00:07:26 +0500

    Предлагаем пользователю ввести массив строк, задаем массив array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+//меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+
+Console.Clear();
+Console.WriteLine("Введите массив строк через запятую: ");
+string[] array;
+string stroka = Console.ReadLine();
+array = stroka.Split(',');
+var newarray = new string[array.Length];
+var i = 0;
+foreach (var index in array )
+{
+    if (index.Length <= 3)
+    {                    
+        newarray[i] = index;
+        i++;
+        Console.WriteLine(index);
+    }
+}
+            
